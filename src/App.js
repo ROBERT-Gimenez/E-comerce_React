6da739c8661d4a1bdc448cefd3310ff70f68d629
ROBERT-Gimenez <robertgim37@gmail.com>
2022-11-28 20:28:35 -0300

    firts commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Home from "./components/Home";
+import Header from "./components/Header";
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
