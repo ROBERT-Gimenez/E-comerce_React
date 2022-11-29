86163d0d58ceede6af60f9baf4e6937e8f4aa31a
ROBERT-Gimenez <robertgim37@gmail.com>
2022-11-28 22:09:15 -0300

    assembly of the header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Home from "./components/Home";
 import Header from "./components/Header";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
 function App() {
   return (
     <div className="App">
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,45 +1,49 @@
 import {NavDropdown , Form , Button ,Navbar ,Nav, Container } from 'react-bootstrap';
-
+import Logo from '../resource/img/logo-2.svg'
 export default function Header() {
   return (
     <header>
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-          <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-        </Navbar.Collapse>
-          </Nav>
+    <Navbar bg="dark" variant="dark">
+    <Container>
+        <Navbar.Brand href="/">
+            <img
+              alt="logo"
+              src={Logo}
+              width="120"
+              height="60"
+              className="d-inline align-top Logo"
+            />{' '}
+        </Navbar.Brand>
+        <Nav className="me-auto nav-container">
+            <Navbar.Collapse id="navbarScroll" className='nav-contein-btn'>
+                <Nav
+                    className="me-auto my-2 my-lg-0"
+                    style={{ maxHeight: '100px' }}
+                    navbarScroll
+                >
+                    <NavDropdown title="Categorias" id="navbarScrollingDropdown">
+                        <NavDropdown.Item href="#action3">Bicicletas</NavDropdown.Item>
+                        <NavDropdown.Item href="#action4">Accesorios</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action5">Promociones</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+                <Form className="d-flex">
+                    <Form.Control
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                    />
+                    <Button variant="outline-success">Search</Button>
+                </Form>
+            <Nav.Link href="#features">carro</Nav.Link>
+            <Nav.Link href="#pricing">Profile</Nav.Link>
+            </Navbar.Collapse>
+        </Nav>
           
-        </Container>
-      </Navbar>
+    </Container>
+    </Navbar>
     </header>
   )
 }
