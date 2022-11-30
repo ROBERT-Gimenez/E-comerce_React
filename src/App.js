97bdeb464d7107874e607af401609020a7de7939
ROBERT-Gimenez <robertgim37@gmail.com>
2022-11-29 23:44:46 -0300

    react router dom added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,20 @@ import Header from "./components/Header";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import Footer from "./components/Footer";
+import {Routes , Route} from 'react-router-dom'
+import Login from "./components/Login/Login";
+import Profile from "./components/Profile/Profile";
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Home/>
+      <Routes>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/login" element={<Login/>}></Route>
+      <Route path="/Profile" element={<Profile/>}></Route>
+      
+
+      </Routes>
       <Footer/>
     </div>
   );
