41af960fa571c979167a55ac46aaac3860ed0dac
ROBERT-Gimenez <robertgim37@gmail.com>
2022-11-29 23:50:36 -0300

    added link paths

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Footer from "./components/Footer";
 import {Routes , Route} from 'react-router-dom'
 import Login from "./components/Login/Login";
 import Profile from "./components/Profile/Profile";
+import Cart from "./components/Cart/Cart";
+import Admin from "./components/Admin/Admin";
 function App() {
   return (
     <div className="App">
@@ -14,6 +16,8 @@ function App() {
       <Route path="/" element={<Home/>}></Route>
       <Route path="/login" element={<Login/>}></Route>
       <Route path="/Profile" element={<Profile/>}></Route>
+      <Route path="/ShoppingCart" element={<Cart/>}></Route>
+      <Route path="/Admin-Profile" element={<Admin/>}></Route>
       
 
       </Routes>
