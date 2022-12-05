932ba81e8cbaeaf9dd1463faec01b7973a718372
ROBERT-Gimenez <robertgim37@gmail.com>
2022-12-04 23:19:04 -0300

    imgages product added

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,26 +1,38 @@
 import {Row , Col , Card , Button} from 'react-bootstrap'
-import img from '../../resource/img/carrousel/banner-4.png'
-import React from 'react'
+import {useState , useEffect} from 'react'
 import {ImHeart} from "react-icons/im";
 import './List.css'
-
+import axios from 'axios';
+const imgs = require.context('../../resource/img/products', true);
 
 export default function List() {
+
+
+    const [productsList , setProductsList] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:4000/api/products")
+        .then(response => {
+            const apiData = response.data;
+            console.log(apiData.data)
+            setProductsList(apiData.data)
+        }).catch(err => alert(err))
+    } ,[])
+/* Array.from({ length: 8 }) */
   return (
     <div>
         <Row xs={1} md={4} className="g-4">
-      {Array.from({ length: 8 }).map((_, idx) => (
+      {productsList.map((prod,idx) => (
         <Col key={idx}>
          <Card style={{ width: '18rem' }} >
             <ImHeart className='cart-card'/>
-            <Card.Img variant="top" src={img} />
+            <Card.Img variant="top" src={imgs(`${prod.image}`)} />
             <Card.Body>
-                <Card.Title>Card Title</Card.Title>
+                <Card.Title>{prod.title}</Card.Title>
                 <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
+                {prod.description}
                 </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <Button variant="primary">Ver</Button>
             </Card.Body>
             </Card>
         </Col>
