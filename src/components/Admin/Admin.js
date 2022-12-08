6841047ca4e3d30dc04afac1529bdc7b86f4bdf7
ROBERT-Gimenez <robertgim37@gmail.com>
2022-12-08 03:43:12 -0300

    admin api call data

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,47 +1,84 @@
-import React from 'react'
- import {Table , Nav} from 'react-bootstrap';
+import {useState , useEffect} from 'react'
+import {Table , Nav} from 'react-bootstrap';
+import style from './Admin.css'
+import useGetAxios from '../../hooks/useGetAxios';
 export default function Admin() {
+  const user = useGetAxios("http://localhost:4000/api/user");
+  const Product = useGetAxios("http://localhost:4000/api/products")
+
+  const [users , setUsers] = useState([]);
+  const [products , setProducts] = useState([]);
+
+  useEffect(() => {
+    user.data && setUsers(user.data)
+  },[user.loading]);
+
+  function getProduct(){
+    Product.data && setProducts(Product.data) 
+    setUsers([])
+  } 
+  function getUsers(){
+    user.data && setUsers(user.data)
+    setProducts([])
+  } 
+
   return (
     <div>
-    <Nav variant="tabs" defaultActiveKey="/home" className='nav-admin'>
+    <Nav variant="tabs" defaultActiveKey="/home" className={style.nav}>
       <Nav.Item>
-        <Nav.Link eventKey="Users">Users</Nav.Link>
+        <Nav.Link eventKey="Users" onClick={getUsers}>Users</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link eventKey="Products">Products</Nav.Link>
+        <Nav.Link eventKey="Products" onClick={getProduct}>Products</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link eventKey="Categorys" >Categorias</Nav.Link>
+        <Nav.Link eventKey="Categorys" >Compras</Nav.Link>
       </Nav.Item>
     </Nav>
     <Table striped bordered hover variant="dark">
       <thead>
-        <tr>
+        {users && users.length > 1 ? 
+          <tr>
           <th>#</th>
           <th>First Name</th>
-          <th>Last Name</th>
-          <th>Username</th>
+          <th>Email</th>
+          <th>phone</th>
         </tr>
+        :
+        <tr>
+          <th>#</th>
+          <th>Product Name</th>
+          <th>description</th>
+          <th>price</th>
+          <th>discount</th>
+          <th>stock</th>
+        </tr>
+        }
       </thead>
       <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td colSpan={2}>Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
+        {!user.loading && users.map((user , indx)=> {
+          return(
+          <tr key={indx}>
+          <td>{user.id}</td>
+          <td>{user.name}</td>
+          <td>{user.email}</td>
+          <td>{user.telefono}</td>
+        </tr>)
+        }) }
+        {!Product.loading && products.map((product , indx)=> {
+          return(
+          <tr key={indx}>
+          <td>{product.id}</td>
+          <td>{product.name}</td>
+          <td>{product.description}</td>
+          <td>{product.price}</td>
+          <td>{product.discount}</td>
+          <td>{product.stock}</td>
+        </tr>)
+        }) }
+
       </tbody>
+        
     </Table>
     </div>
   )
