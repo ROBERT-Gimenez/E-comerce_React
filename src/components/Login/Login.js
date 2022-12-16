b30921caa1778c06217224af18f78e2c4913e25a
ROBERT-Gimenez <robertgim37@gmail.com>
2022-12-15 23:25:58 -0300

    dependence edit

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,19 +1,61 @@
 import React from 'react'
 import '../Login/Login.css'
+import axios from 'axios'
+import {useNavigate} from 'react-router-dom'
+
+
+
+   
+  
 export default function Login() {
+
+    
+    const history = useNavigate();
+    
+    
+    const handleSubmit = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    
+    const regexEmail =/^(([^<>()\]\\.,;:\s@"]+(\.[^<>()\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    /* if(email === "" || password === ""){
+        swal(<h2>Los campos no pueden estart vacios</h2>)
+        return;
+        }
+    if(email !== '' && !regexEmail.test(email)){
+        swal(<h2>Ingrese un email valido</h2>)
+        }
+    
+    if(email !== 'challenge@alkemy.org' || password !== 'react'){
+        swal(<h2>Credenciales Invalidas</h2>)
+        }
+            
+    axios.post('http://challenge-react.alkemy.org' , {email,password})
+        .then(res => {
+        swal(<h2>Perfecto,Ingresaste Correctamente</h2>)
+        const tokenAdquirido = res.data.token;
+        localStorage.setItem('token' , tokenAdquirido);// localStorage.getItem('token')
+        history("/");//redireccionamos la pagina con useNavigate
+        }).catch((err) => { console.log(err)})
+    
+             */
+        }
+    
   return (
-    <form className='body-login' > 
+    <>
     <div className="container-form">
-        <div className="login-container">
+    <div className="login-container">
+    <form className='body-login' onSubmit={handleSubmit} > 
             <input id="item-1" type="radio" name="item" className="sign-in" defaultChecked={true}/><label htmlFor="item-1" className="item">Sign In</label>
             <input id="item-2" type="radio" name="item" className="sign-up"/><label htmlFor="item-2" className="item">Sign Up</label>
             <div className="login-form">
                 <div className="sign-in-htm">
                     <div className="group">
-                        <input placeholder="Username" id="user" type="text" className="input"/>
+                        <input placeholder="Username" id="user" name='email' type="email" className="input"/>
                     </div>
                     <div className="group">
-                        <input placeholder="Password" id="pass" type="password" className="input" data-type="password"/>
+                        <input placeholder="Password" id="pass" name='password' type="password" className="input" data-type="password"/>
                     </div>
 
                     <div className="group">
@@ -24,6 +66,7 @@ export default function Login() {
                         <a href="#forgot">Forgot Password?</a>
                     </div>
                 </div>
+            
                 <div className="sign-up-htm">
                     <div className="group">
                         <input placeholder="Username" id="user_sing_up" type="text" className="input"/>
@@ -49,8 +92,9 @@ export default function Login() {
 				</div>
 			</div>
 		</div>
-	</div>
-</div>
     </form>
+	</div>
+    </div>
+    </>
   )
 }
