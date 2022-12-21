395ed085c59f3208331f1ba7c101fbf43e68f5bb
ROBERT-Gimenez <robertgim37@gmail.com>
2022-12-20 23:25:47 -0300

    rutes api added for login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,13 +12,27 @@ export default function Login() {
     
     const history = useNavigate();
     
-    
+    const verifique = (e) => {
+        const email =e.target.value;
+        console.log(email)
+        fetch('http://localhost:4000/api/user')
+        .then(response => response.json())
+        .then(result => {
+            console.log(result.data)
+            result.data.forEach(element => {
+                if(email === element.email){
+                    alert("email ya registrado")
+                    }
+                   
+                
+            });
+        })
+    }
     const handleSubmit = (e) => {
     e.preventDefault();
     const email = e.target.email.value;
     const password = e.target.password.value;
-    console.log("email = " + email)
-    console.log("password = " + password)
+    
     const regexEmail =/^(([^<>()\]\\.,;:\s@"]+(\.[^<>()\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
      if(email === "" || password === ""){
         alert("Los campos no pueden estart vacios")
@@ -42,18 +56,44 @@ export default function Login() {
     
              
         }
+    const submitCreate = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    
+    const regexEmail =/^(([^<>()\]\\.,;:\s@"]+(\.[^<>()\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+     if(email === "" || password === ""){
+        alert("Los campos no pueden estart vacios")
+        return;
+        }
+    if(email !== '' && !regexEmail.test(email)){
+        alert("Ingrese un email valido")
+        }
+            
+    axios.post('http://localhost:4000/api/user/create' , {email,password})
+        .then(res => {
+        alert("Perfecto,Ingresaste Correctamente")
+        const tokenAdquirido = res.data.token;
+        localStorage.setItem('token' , tokenAdquirido);// localStorage.getItem('token')
+        history("/");//redireccionamos la pagina con useNavigate
+        }).catch((err) => { console.log(err)})
+    
+             
+        }
+
+
     
   return (
     <>
     <div className="container-form">
     <div className="login-container">
-            <input id="item-1" type="radio" name="item" className="sign-in" defaultChecked={true}/><label htmlFor="item-1" className="item">Sign In</label>
-            <input id="item-2" type="radio" name="item" className="sign-up"/><label htmlFor="item-2" className="item">Sign Up</label>
-    <form className='body-login' onSubmit={handleSubmit} > 
+            <input id="item-1" type="radio" name="item"  className="sign-in" defaultChecked={true}/><label htmlFor="item-1" className="item">Sign In</label>
+            <input id="item-2" type="radio" name="item"  className="sign-up"/><label htmlFor="item-2" className="item">Sign Up</label>
+    <form className='body-login'  onSubmit={handleSubmit} > 
             <div className="login-form">
                 <div className="sign-in-htm">
                     <div className="group">
-                        <input placeholder="Username" id="user" name='email' type="email" className="input"/>
+                        <input onKeyDown={verifique} placeholder="Username" id="email " name="email" type="email" className="input"/>
                     </div>
                     <div className="group">
                         <input placeholder="Password" id="pass" name='password' type="password" className="input" data-type="password"/>
@@ -67,9 +107,10 @@ export default function Login() {
                         <a href="#forgot">Forgot Password?</a>
                     </div>
                 </div>
-                </div>
+            </div>
                 </form>
-                <form>
+                <form className='sign-up-form'>
+                <div className="login-form">   
                  <div className="sign-up-htm">
                     <div className="group">
                         <input placeholder="Username" id="user_sing_up" type="text" className="input"/>
@@ -91,9 +132,10 @@ export default function Login() {
                     </div>
                     <div className="hr"></div>
                     <div className="footer">
-                        <label htmlFor="item-1">Already have an account?</label>
+                        <label className='label' htmlFor="item-1">Already have an account?</label>
 				</div>
 			</div> 
+            </div> 
     </form>
 	</div>
     </div>
